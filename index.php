7dcd7bdba83e8acc0b299c13adaa1153884d8d65
Roy Vanegas <roy@thecodeeducators.com>
2024-10-30 12:16:19 -0400

    Use a PHP variable for the title and h1 elements

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
+<?php $title = "Lamp Stack Test"; ?>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>LAMP Stack Test</title>
+  <title><?php echo $title ?></title>
 </head>
 <body>
-
+  <header>
+    <h1><?php echo $title ?></h1>
+  </header>
 </body>
 </html>
